c5b448152e109aca037c4857079185839e33a272
morgbaker <118389113+morgbaker@users.noreply.github.com>
2024-10-10 17:05:15 +0000

    changing wording in all tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,15 @@ model = AutoModelForSequenceClassification.from_pretrained("mohameddhiab/humor-n
 st.set_page_config(page_title="Humor Detection App", layout="centered")
 st.title("🤖 Humor Detection with Transformers")
 
-# Create tabs in the desired order
+# Create tabs
 tabs = st.tabs(["😂 Enter a Joke", "📖 About the Model", "🎓 Credits"])
 
 # Enter a Joke tab
 with tabs[0]:
     st.header("Test Your Jokes")
-    st.subheader("Ever wanted to crack a joke but didn't know if your friends would laugh? Now you can find out if your joke is funny before!")
-    
+    st.subheader("Ever wondered if your joke would land? Now you can test your ideas and discover if your joke is funny before you share!")
+    with st.expander("Disclaimer", expanded=False):
+        st.write("Please note that the humor detection model may not always provide accurate results. The creators are not responsible for any embarrassment caused by jokes that may not land as intended.")
     # Text input from user
     input_text = st.text_input("💬 Enter your joke:", "")
 
@@ -48,33 +49,61 @@ with tabs[0]:
         else:
             st.warning("😐 Not quite a joke! Better luck next time!")
 
+
 # About the Model tab
 with tabs[1]:
     st.header("About the Model")
+
     st.write("### DistilBERT")
     st.write("""
         DistilBERT is a smaller, faster, and lighter version of BERT (Bidirectional Encoder Representations from Transformers). 
-        It retains 97% of BERT's language understanding while being 60% faster and reducing the model size by 40%. 
-        This model is particularly useful for tasks such as sentiment analysis, question answering, and text classification.
+        Developed by Hugging Face, it retains **97% of BERT's language understanding** while being **60% faster** and reducing the model size by **40%**. 
+        This efficiency makes DistilBERT particularly suitable for applications where computational resources are limited or where quick responses are essential, such as:
     """)
-    st.write("This model has been fine-tuned on a joke/no-joke dataset to effectively detect humor in text. See more specifics about the model used at [Humor Detection Model](https://huggingface.co/mohameddhiab/humor-no-humor)")
     
+    st.write("""
+        - **Sentiment Analysis**: Understanding the sentiment conveyed in text data.
+        - **Question Answering**: Providing precise answers to user queries.
+        - **Text Classification**: Categorizing text into predefined labels.
+    """)
+
+    st.write("### Humor Detection")
+    st.write("""
+        This model has been **fine-tuned on a joke/no-joke dataset** to effectively detect humor in text. By leveraging the power of DistilBERT, the humor detection model can analyze linguistic patterns, context, and sentiment to discern whether a given piece of text is humorous or not.
+        
+        For more details about the specific model used, check out the [Humor Detection Model](https://huggingface.co/mohameddhiab/humor-no-humor).
+    """)
+
+    st.write("### Limitations and Things to Keep in Mind")
+    st.write("""
+        While DistilBERT is powerful, there are several limitations to consider when using this model for humor classification:
+        
+        - **Contextual Understanding**: Humor is often context-dependent, and the model may struggle to capture nuanced cultural references or context-specific humor.
+        
+        - **Subjectivity**: Humor is inherently subjective; what one person finds funny, another may not. The model's training data may not cover all variations of humor, leading to potential misclassifications.
+        
+        - **Limited Dataset**: The effectiveness of the model depends on the quality and diversity of the dataset it was fine-tuned on. This means the model may not generalize well to new or different types of humor.
+        
+        - **Language Nuances**: Sarcasm, irony, and wordplay can be particularly challenging for AI models to interpret correctly. The model may not always accurately identify these forms of humor.
+    """)
+
+    st.markdown("---")  # Add a horizontal line for separation
+
 
 # Credits tab
 with tabs[2]:
     st.header("About Me")
-    col1, col2 = st.columns([1, 2])  # Create two columns for layout
+    col1, col2 = st.columns([1, 2])  
     
     with col1:
-        # Display your permanent image
-        image_path = "headshot.jpg"  # Replace with the name of your image file
+        image_path = "headshot.jpg"  
         image = Image.open(image_path)
         st.image(image, caption='Morgan Baker', use_column_width=True)
 
     with col2:
-        st.write("👋 Hello! I'm an undergraduate student studying Data Science* and Economics. I made this app for my coursework and because my jokes often flop.")
-    
-    st.markdown("---")  # Add a horizontal line for separation
+        st.write("👋 Hello! I'm an undergraduate student studying Data Science and Economics at West Virginia University. I am interested in humor text analysis and machine learning applications. Please feel free to reach out with any questions or feedback!")
+        st.write("🔗 LinkedIn: [Connect with me!](https://www.linkedin.com/in/morgan-baker-1a358b265/)")
+    st.markdown("---")  
     st.write("### Credits")
     st.write("App Developed by: Morgan Baker")
     st.write("Model used: [Humor Detection Model](https://huggingface.co/mohameddhiab/humor-no-humor)") 
